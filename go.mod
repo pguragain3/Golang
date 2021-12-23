31e7d201f5c867acf83ab1d098a41b4764159940
Prabesh Guragain <pguragain3@gmail.com>
2021-12-23 22:20:01 +0545

    added stuffs

---
+++ go.mod
@@ -0,0 +1,5 @@
+module basics
+
+go 1.13
+
+require rsc.io/quote/v3 v3.1.0
---
+++ helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote/v3"
+)
+
+func main() {
+	fmt.Println("Hello World From Golang !!")
+	fmt.Println(quote.GoV3())
+}
